6f9d764611568e025261a1e86fe2d6863882c6f7
Hrust <96692587+Hruster1@users.noreply.github.com>
2023-05-21 14:27:36 +0300

    Update GameController.cs

--- GameController.cs
+++ GameController.cs
@@ -9,7 +9,7 @@ public class GameController : MonoBehaviour
     [Range(0,5)]
     public float gameSpeedRegulator;
     public float speedRate = 0.5f;
-    public float gameSpeedMax = 5;
+    public float gameSpeedMax = 20;
 
     // Update is called once per frame
     void Update()
